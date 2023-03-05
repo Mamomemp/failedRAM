0d1c6af8e06e1e3e8e42e3732043b2f851c6aee4
Mamomemp <111734778+Mamomemp@users.noreply.github.com>
2023-03-06 00:56:06 +0100

    Player Movement leap
    teil 2
    benutzt curves
    funktioniert nur im ersten frame und dan tp es sich zum Ziel.

--- failedRAM/Assets/Scripte/PlayerMovement.cs
+++ failedRAM/Assets/Scripte/PlayerMovement.cs
@@ -3,31 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    public Transform target;
-    public float target_speed;
-    public float speed;
-    public float lauf_coldown;
+{    
     public Vector3 start_position;
+    public Transform target;
+
+    public float target_sprung_weite;    
+    public float lauf_coldown;
+    public float gewollteZeitDauer;
+    public AnimationCurve curve;
+    private float vergangene_Zeit;
+
     private void Start()
     {
-        target.parent = null;
+        target.parent = null;    
         start_position = transform.position;
+
     }
 
     private void Update()
-    {   //Bewegung
-        transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+    {     
+
+        //Bewegung
+        vergangene_Zeit += Time.deltaTime;
+        float percentageComplete = vergangene_Zeit / gewollteZeitDauer;
+        transform.position = Vector3.Lerp(start_position, target.position, curve.Evaluate(percentageComplete));
         //Coldown
         if (Vector3.Distance(transform.position, target.position) <= lauf_coldown)
         {
             //Input
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
-            {
-                target.position += new Vector3(Input.GetAxisRaw("Horizontal")*target_speed, 0f, 0f);
-            }else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
-            {
-                target.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical")*target_speed);
+            {                start_position = transform.position;
+
+                target.position += new Vector3(Input.GetAxisRaw("Horizontal")*target_sprung_weite, 0f, 0f);
+
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            {                start_position = transform.position;
+
+                target.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical")*target_sprung_weite);
+
             }
         }
     }
