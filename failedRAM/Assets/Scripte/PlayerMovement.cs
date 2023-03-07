fe1d92c9a3af7d0af644ba9d95b1f6828fcbc4e0
Mamomemp <111734778+Mamomemp@users.noreply.github.com>
2023-03-07 20:44:29 +0100

    Player Movement Funktionier Einwand Frei YAY
    Es kann Kollisionen erkennen, man kann die Sprungweite einstellen und die lauf geschwindichkeit.
    Der Target bewegt sich auf ein Grid und der Spieler geht auf direktem weg auf das Target zu.

--- failedRAM/Assets/Scripte/PlayerMovement.cs
+++ failedRAM/Assets/Scripte/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public float target_sprung_weite;
     public float spieler_geschwindichkeit;
     public float lauf_coldown;
+    
 
     public LayerMask barriere_Layer;
-    private Vector3 target_neue_position = target.position + new Vector3(Input.GetAxisRaw("Horizontal") * target_sprung_weite, 0f, 0f); //Test
 
     private void Start()
     {
@@ -22,20 +22,22 @@ public class PlayerMovement : MonoBehaviour
         //Bewegung
         transform.position = Vector3.MoveTowards(transform.position, target.position, spieler_geschwindichkeit*Time.deltaTime);
 
+
         //Coldown
         if (Vector3.Distance(transform.position, target.position) <= lauf_coldown)
         {
             //Input
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
             {
-                if (!Physics.OverlapSphere(target.position + new Vector3(Input.GetAxisRaw("Horizontal") * target_sprung_weite, 0f, 0f), .2f, barriere_Layer))
+                if (!Physics.CheckSphere(target.position + new Vector3(Input.GetAxisRaw("Horizontal") * target_sprung_weite, 0f, 0f), .5f, barriere_Layer))
                 {
                     target.position += new Vector3(Input.GetAxisRaw("Horizontal") * target_sprung_weite, 0f, 0f);
                 }
             }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            //Um quer movement zu stoppen muss man else if machen 
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
             {
-                if (Physics.OverlapSphere(target.position + new Vector3(Input.GetAxisRaw("Vertical") * target_sprung_weite), .2f, barriere_Layer))
+                if (!Physics.CheckSphere(target.position + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * target_sprung_weite), .2f, barriere_Layer))
                 {
                     target.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * target_sprung_weite);
                 }
