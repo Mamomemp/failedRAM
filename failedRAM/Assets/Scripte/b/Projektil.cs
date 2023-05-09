eed0c4616d9b3439a830f418715a569c02d5c37b
Mamomemp <111734778+Mamomemp@users.noreply.github.com>
2023-05-09 17:48:42 +0200

    33% des Wüsten Level
    Projektile sogut wie fertig

--- failedRAM/Assets/Scripte/b/Projektil.cs
+++ failedRAM/Assets/Scripte/b/Projektil.cs
@@ -18,10 +18,18 @@ public class Projektil : MonoBehaviour
     {
         transform.position += transform.forward * _speed * Time.deltaTime;
 
-        _lebensZeit -= Time.deltaTime;
+       /* _lebensZeit -= Time.deltaTime;
         if (_lebensZeit <= 0f)
+        {
+            bulletPool.ReturnBullet(gameObject);
+        }*/
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Lösche_Projektil" && LayerMask.LayerToName(collision.gameObject.layer) == "Barriere")
         {
             bulletPool.ReturnBullet(gameObject);
         }
     }
+
 }
