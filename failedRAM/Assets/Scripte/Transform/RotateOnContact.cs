de42aade3f1241fd441cdedac7949b42cff94b64
Mamomemp <111734778+Mamomemp@users.noreply.github.com>
2023-07-18 08:38:23 +0200

    Und Weiter Geht es
    Fertig: Bullet Collison mit Stienen
    Noch zu Machen: Wurm Model austauschen, Sandsturm, Game Feel

--- failedRAM/Assets/Scripte/Transform/RotateOnContact.cs
+++ failedRAM/Assets/Scripte/Transform/RotateOnContact.cs
@@ -7,6 +7,11 @@ public class RotateOnContact : MonoBehaviour
     [SerializeField]
     private bool hasCollided = false; // Flag to track collision
 
+    [SerializeField]
+    private float rotationDuration = 0.5f; // Duration for which the object remains rotated
+
+    private Coroutine rotationCoroutine; // Reference to the coroutine
+
     private void OnCollisionEnter(Collision collision)
     {
         // Check if colliding with an object with the "Indikator_aktivierer" tag and hasn't collided before
@@ -14,6 +19,9 @@ public class RotateOnContact : MonoBehaviour
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
             hasCollided = true;
+
+            // Start the rotation timer
+            rotationCoroutine = StartCoroutine(RotateBack());
         }
     }
 
@@ -24,7 +32,19 @@ public class RotateOnContact : MonoBehaviour
         {
             transform.localRotation = Quaternion.Euler(180, 0, 0);
             hasCollided = false;
+
+            // Stop the rotation timer if it is running
+            if (rotationCoroutine != null)
+                StopCoroutine(rotationCoroutine);
         }
+    }
+
+    private IEnumerator RotateBack()
+    {
+        yield return new WaitForSeconds(rotationDuration);
+
+        transform.localRotation = Quaternion.Euler(180, 0, 0);
+        hasCollided = false;
     }
 }
 
