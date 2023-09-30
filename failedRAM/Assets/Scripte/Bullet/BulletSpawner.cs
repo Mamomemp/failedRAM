4942496484eec22e3bd42d6423060a29e8d54a1b
Mamomemp <111734778+Mamomemp@users.noreply.github.com>
2023-10-01 00:45:37 +0200

    fixed Bullets
    Changed the Bullet Spawn Script.
    Ind und Bullet Spawn kann jetzt über ein componente gesteuert werden und habe ein paar debug tools dem Script hinzugefügt.
    
    Die Timeline / Bullets haben nun keine probleme mehr.
    Die Indikatoren der Würmer und der Bullets sollten noch angepasst werden!

--- failedRAM/Assets/Scripte/Bullet/BulletSpawner.cs
+++ failedRAM/Assets/Scripte/Bullet/BulletSpawner.cs
@@ -1,18 +1,43 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
     [SerializeField] private BulletPool bulletPool;
+    [SerializeField] private BulletPool indPool;
     private Vector3 spawnPosition;
     [SerializeField] private float xRotation;
     [SerializeField] private float yRotation;
     [SerializeField] private float zRotation;
 
-    [SerializeField] private int derzeitigeElement = 0;
+    [SerializeField] private int bulletElement = 0;
+    [SerializeField] private int indElement = 0;
+    [SerializeField] private bool printBullets = false;
+
     public List<Vector3> transformList;
-    public void SpawnBullet()
+
+    public void spawnBullet()
+    {
+        Spawn(bulletPool, ref bulletElement);
+        if (printBullets == true)
+        {
+            if (bulletElement < transformList.Count)
+            {
+                print(bulletElement + " " + transformList[bulletElement]);
+            }
+            else
+            {
+                print("No more :D");
+            }
+        }
+    }
+
+    public void spawnInd()
+    {
+        Spawn(indPool, ref indElement);
+    }
+    public void Spawn(BulletPool bP, ref int derzeitigeElement)
     {
         if (derzeitigeElement < transformList.Count)
         {
@@ -21,10 +46,12 @@ public class BulletSpawner : MonoBehaviour
         }
         else
         {
-            derzeitigeElement = 0;
+           // derzeitigeElement = 0;
+       
+           
         }
 
-        GameObject bullet = bulletPool.GetBullet();
+        GameObject bullet = bP.GetBullet();
 
         bullet.transform.position = spawnPosition;
         bullet.transform.rotation = Quaternion.Euler(xRotation, yRotation, zRotation);
