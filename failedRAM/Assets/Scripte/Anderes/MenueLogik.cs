bde18d680f52d2703d31a4c3570759f58ee6c6a0
Mamomemp <111734778+Mamomemp@users.noreply.github.com>
2023-11-09 14:20:47 +0100

    Yeah Stuff

--- failedRAM/Assets/Scripte/Anderes/MenueLogik.cs
+++ failedRAM/Assets/Scripte/Anderes/MenueLogik.cs
@@ -5,12 +5,7 @@ using UnityEngine.SceneManagement;
 public class MenueLogik : MonoBehaviour
 {
     private bool secret_level_start = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
